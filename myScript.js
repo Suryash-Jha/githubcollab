3a90a807e08a127f34471bde558cfae22234ce1f
jharaghav32 <jharaghav3209@gmail.com>
2022-12-09 17:37:50 +0530

    made some changes

--- myScript.js
+++ myScript.js
@@ -14,20 +14,26 @@
             const options = {method: 'GET'};
             var Todate= localStorage.getItem("date");
             if(Todate== getCurrDate()){
-                document.querySelector("#date").innerHTML= `Date: ${Todate}`;
+                document.querySelector("#date").innerHTML= `${Todate}`;
                 for(let i=0; i<4; i++){
                         var content= localStorage.getItem(`question${i+1}`);
-                        document.querySelector(`#question${i+1}`).innerHTML= `Question ${i+1}: ${content}`;
+                        console.log(content)
+                        var a = document.createElement('a');
+                        a.setAttribute("href",`${content}`);
+                        a.setAttribute("target","_blank")
+                        a.innerText=`${content}`
+                        var div=document.querySelector(`#question${i+1}`);
+                        console.log(div);
+                        div.appendChild(a);
                     }
             }
             else{
                 fetch('https://script.google.com/macros/s/AKfycbwkHlGDRJbaumjp0FkSZeCOEirpC6Rqgf0-JhF45dLeh6eRSLuCNp0usVQjbN7wkSb9/exec', options)
             .then(response => response.json())
             .then(response => {
-                console.log("work");
+                console.log(response);
                 let currDate= getCurrDate();
                 const todayQuestions= response['data'][response['data'].length-1];
-                // consolelog(todayQuestions);
                 console.log(todayQuestions);
                 const lastDateKey= Object.keys(todayQuestions);
                 if(lastDateKey== currDate){
